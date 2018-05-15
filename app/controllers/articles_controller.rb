aa222ef0fddd1b7b88f2af95f8f228025fabf0f6
shanpu <cheztomo513@gmail.com>
2018-05-15 19:41:48 +0900

    自分の作った記事だけ消せるように設定、記事表示のページを整えた

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,55 @@
 class ArticlesController < ApplicationController
+
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = current_user.articles.build
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to root_url
+    else
+      redirect_to root_url
+    end
+  end
+
+  def show
+    @article = Article.find_by(params[:id])
+  end
+
+  def edit
+    @artic
+  end
+
+  def update
+    @article = Article.find_by(params[:id])
+    if @article.update_attributes(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find_by(params[:id])
+    @article.destroy
+    redirect_to root_url
+  end
+
+  private
+
+  # strong params
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def correct_user
+    @article = Article.find_by(params[:id])
+    redirect_to root_url unless current_user == @article.user
+  end
 end
